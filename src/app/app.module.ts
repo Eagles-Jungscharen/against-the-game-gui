9ee846891421e27c4d417a825ce830586f6f495a
Christian Guedemann <christian.guedemann@webgate.biz>
2021-09-26 07:10:49 +0200

    logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 import { RunGameComponent } from './run-game/run-game.component';
 import { EditGameComponent } from './edit-game/edit-game.component';
 import { LoginGameComponent } from './login-game/login-game.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     RunGameComponent,
     EditGameComponent,
-    LoginGameComponent
+    LoginGameComponent,
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/login-game/login-game.component.ts
+++ src/app/login-game/login-game.component.ts
@@ -1,15 +1,68 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+enum LoginStatus {
+  START,
+  EDIT,
+  CREATE,
+  REGISTER,
+}
 
 @Component({
   selector: 'app-login-game',
   templateUrl: './login-game.component.html',
-  styleUrls: ['./login-game.component.scss']
+  styleUrls: ['./login-game.component.scss'],
 })
 export class LoginGameComponent implements OnInit {
+  private status: LoginStatus = LoginStatus.START;
+  codeJoinGameControl = new FormControl('', checkCodeFormat());
+  codeEditGameControl = new FormControl('', checkCodeFormat());
+  codeEditVerificationGameControl = new FormControl('', checkCodeFormat());
+  registerEMailControl = new FormControl('');
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  startButtonDisabled(): boolean {
+    return this.status == LoginStatus.EDIT || this.codeJoinGameControl.invalid;
+  }
+  newButtonDisabled(): boolean {
+    return this.status != LoginStatus.START;
+  }
+  editButtonDisabled(): boolean {
+    return this.status == LoginStatus.EDIT || this.codeEditGameControl.invalid;
+  }
+  cancelButtonDisabled(): boolean {
+    return this.status == LoginStatus.START;
+  }
+  showEMailAddress(): boolean {
+    return this.status == LoginStatus.CREATE;
+  }
+  showVerification(): boolean {
+    return false;
+  }
+  showEditButton(): boolean {
+    return this.status == LoginStatus.START;
   }
 
+  doCancel() {
+    this.status = LoginStatus.START;
+  }
+  doJoinGame() {}
+  doNewGame() {
+    this.status = LoginStatus.CREATE;
+  }
+  doEditGame() {
+    this.status = LoginStatus.EDIT;
+  }
+}
+
+export function checkCodeFormat(): ValidatorFn {
+  const nameRex = new RegExp('\\d\\d\\d-\\d\\d\\d-\\d\\d\\d$');
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value = control.value + '';
+    const valid = nameRex.test(value);
+    return valid ? null : { chekCodeFormat: { value: control.value } };
+  };
 }
